6787db37254a34624fc23af780090980b9e39684
jeongxoo <56291884+jeongxoo@users.noreply.github.com>
2020-11-26 22:20:20 +0900

    성공~~

--- NumBot/Assets/Scripts/ML.cs
+++ NumBot/Assets/Scripts/ML.cs
@@ -7,7 +7,7 @@ public class ML : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 강화학습을 구현해낼 수 있을까?
     }
 
     // Update is called once per frame
