d004e984818d922c370d668af4028528e88a6ed5
jeongxoo <56291884+jeongxoo@users.noreply.github.com>
2020-11-26 21:27:51 +0900

    넘보트 최초 업로드
    넘보트 프로토 타입 업로드

---
+++ NumBot/Assets/Scripts/Magic.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    public GameObject shield;
+    public int s_count = 0;
+    public int s_Destroy = 5;
+    public GameObject fireBall;
+
+    private void Update()
+    {
+        DestroyUnitWhenTimeOver();
+        if (s_count >= s_Destroy)
+            Destroy(shield);
+    }
+
+    public void DestroyUnitWhenTimeOver() // 게임버이후에 마법이 들이박는걸 막기 위한 함수
+    {
+        if (GameManager.instance.gTime <= 0 || GameManager.instance.eHP <= 0)
+        {
+            Destroy(shield);
+            Destroy(fireBall);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (CompareTag("Shield"))
+        {
+            //Debug.Log("cursh on shield");
+            switch (other.tag)
+            {
+                case "Man1":
+                    break;
+
+                case "Man2":
+                    s_count++;
+                    break;
+
+                case "Man3":
+                    s_count++;
+                    break;
+
+                case "Man4":
+                    s_count++;
+                    break;
+
+                case "Man5":
+                    s_count++;
+                    break;
+            }
+        }
+        else if (CompareTag("Fire"))
+        {
+            //Debug.Log("cursh on fireball");
+            switch (other.tag)
+            {
+                case "Man1":
+                    Destroy(fireBall);
+                    break;
+
+                case "Man2":
+                    Destroy(fireBall);
+                    break;
+
+                case "Man3":
+                    Destroy(fireBall);
+                    break;
+
+                case "Man4":
+                    Destroy(fireBall);
+                    break;
+
+                case "Man5":
+                    Destroy(fireBall);
+                    break;
+
+                case "Ground":
+                    Destroy(fireBall);
+                    break;
+            }
+        }
+        
+
+    }
+
+}
