2514fafba6087b81ab940647c88eafb0e522402a
jeongxoo <56291884+jeongxoo@users.noreply.github.com>
2020-12-01 22:23:30 +0900

    Audio & KNN
    오디오 매니저 추가
    KNN 1차 추가

---
+++ NumBot/Assets/Scripts/Audio.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    // 싱클톤 패턴
+    #region 싱클톤패턴
+    static public Audio instance; // 싱글톤 패턴을 이용한 오디오 매니저 생성을 위해 클래스 자체를 인스턴스화
+
+    void Awake() // 최초 실행
+    {
+        if (instance == null) // 인스턴스가 null이라면 (초기화 X)
+        {
+            instance = this; // 인스턴스에 자기 자신을 초기화
+            DontDestroyOnLoad(gameObject); // 최초 1회 생성된 인스턴스를 파괴하지 않고 계속 사용하기 위해
+        }
+        else // 인스턴스가 null이 아니라면 (초기화 O)
+        {
+            Destroy(gameObject); // 인스턴스가 존재하기 떄문에 새로 생성되는 인스턴스(오디오 매니저)는 파괴
+        }
+    }
+    #endregion 싱클톤패턴
+
+    private void Update()
+    {
+        PlayBGM(BGM);
+    }
+
+    // 오디오 재생을 위한 변수들
+    #region 오디오 재생을 위한 변수들
+
+    public AudioSource[] audioSourceEffects; // 효과음 > 한번에 여러개가 재생될수 있기때문에 배열
+    public AudioSource audioSourceBGM; // 배경음 > 한번에 한곡씩 재생되기때문에 배열 X
+
+    public string playingBGMName; // 현재 재생중인 배경음악의 이름 반환을 위함
+
+    public Sound[] effectSounds;  // 여러 효과음을 담기 위해 배열로
+    public Sound bgmSounds; // 여러 배경음을 담기 위해 배열로
+
+    public string BGM;
+
+    public string Button1;
+
+    public string Button2;
+
+    #endregion 오디오 재생을 위한 변수들
+
+    // 효과음용 함수들
+    #region 효과음용 함수들
+    // 효과음 재생을 위한 함수
+    public void PlayEffectSound(string _name) // sound 클래스의 name과 일치여부를 판단하기 위해 _name을 입력받음
+    {
+        for (int i = 0; i < effectSounds.Length; i++) // 보유한 효과음들 중에 아래 조건을 만족하는게 있는지 파악하기 위한 반복문
+        {
+            if (_name == effectSounds[i].name) // 입력받은 이름이 보유한 효과음 목록에 존재하는지 판단
+            {
+                for (int j = 0; j < audioSourceEffects.Length; j++)
+                {
+                    if (!audioSourceEffects[j].isPlaying) // 재생중인 소스가 없다면 참, 있다면 거짓
+                    {
+                        //playingSoundName[j] = effectSounds[j].name;
+                        Debug.Log("start play se");
+                        audioSourceEffects[j].clip = effectSounds[i].clip;
+                        audioSourceEffects[j].Play();
+                        return;
+                    }
+                    else
+                    {
+                        audioSourceEffects[j + 1].clip = effectSounds[i].clip;
+                        audioSourceEffects[j + 1].Play();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void PlayButton1()
+    {
+        PlayEffectSound(Button1);
+    }
+
+    public void PlayButton2()
+    {
+        PlayEffectSound(Button2);
+    }
+
+    #endregion 효과음용 함수들
+
+    // BGM용 함수들
+    #region BGM용 함수들
+
+    // 배경음을 재생하기 위한 함수
+    public void PlayBGM(string _name) // 위와 마찬가지로 이름을 입력받음
+    {
+        if (bgmSounds.name == _name) // 입력받은 이름이 보유한 배경음 목록에 존재하는지 판단
+        {
+            if (audioSourceBGM.isPlaying) // 노래중에 재생중인 노래가 있는지 판단 / 있으면 참, 없으면 거짓
+            {
+                if (playingBGMName == _name) // 재생중인 노래가 있다면 재생중인 노래의 이름과 입력한 노래의 이름을 비교 / 같으면 참, 다르면 거짓
+                {
+                    return;
+                }
+                else
+                {
+                    playingBGMName = bgmSounds.name;
+                    audioSourceBGM.Stop();
+                    audioSourceBGM.clip = bgmSounds.clip;
+                    audioSourceBGM.Play();
+                    return;
+                }
+
+                //그러면 노래가 재생중인게 있으면 그게 내가 입력한 이름의 것과 같으면 그냥 두고
+                // 같지 않다면 재생중인 곡을 중지하고 내가 입력한것을 재생하게
+            }
+            else // 아무 음악도 재생중이지 않다면 입력받은 이름을 가진 오디오 클립을 재생
+            {
+                playingBGMName = bgmSounds.name;
+                audioSourceBGM.clip = bgmSounds.clip;
+                audioSourceBGM.Play();
+                return;
+            }
+        }
+
+    }
+    #endregion BGM용 함수들
+}
+
+[System.Serializable]
+public class Sound // 음악 재생에 사용될 클래스
+{
+    public string name; // 곡 이름
+    public AudioClip clip; // 곡
+}
--- NumBot/Assets/Scripts/ML.cs
+++ NumBot/Assets/Scripts/ML.cs
@@ -1,18 +1,189 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ML : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public DataSet UserData; // 유저의 플레이 데이터를 모음
+    public List<float> distance; // 계산된 거리 데이터를 모으기 위한 리스트
+    public List<DataSet> TrainData; // 학습데이터를 가져오기 위한 리스트
+
+    public int kNumber; // 사용자가 설정하는 K개의 데이터를 뽑을때 쓰는 변수
+
+    public float bestLabel; // 거리 계산 후 최적의 레이블을 담기 위함
+    public float bestDistance; // 가장 가까운 거리를 갱신하기 위해 필/
+
+    public float[] labelSet; // k개의 레이블들을 담기 위해
+    public int countNumber; // 몇번째 거리, 몇번째 트레인데이터 등을 확인하기 위해
+
+    public float[] saveTime; //데이터를 죽인다음 다시 살리기 위해
+    public int[] saveHP; //데이터를 죽인다음 다시 살리기 위해
+    public int[] saveEnergyProduction; //데이터를 죽인다음 다시 살리기 위해
+    public int[] saveEnergy; //데이터를 죽인다음 다시 살리기 위해
+    public int[] saveShield;
+    public float[] saveResult;
+
+    public float addResult; // 예측값의 평균을 계산하기위해 예측값을 다 담아주는 변수
+
     void Start()
     {
-        // 강화학습을 구현해낼 수 있을까?
+        TrainData = new List<DataSet>();
+        distance = new List<float>();
+
+        kNumber = 3; // 사용자 지정 k
+
+        labelSet = new float[kNumber];
+
+        saveTime = new float[kNumber];
+        saveHP = new int[kNumber];        
+        saveEnergyProduction = new int[kNumber];
+        saveEnergy = new int[kNumber];
+        saveShield = new int[kNumber];
+        saveResult = new float[kNumber];
+
+        ReadData(); // 데이터 읽어오는 함수 실행
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReadData()
     {
-        
+        TrainData.Clear();
+        TextAsset textFile = Resources.Load("Train") as TextAsset; //리소스 폴더안의 트레인 파일을 가져 
+        StringReader stringReader = new StringReader(textFile.text); // 파일열기
+
+        while (stringReader != null) //데이터가 존재한다면 계속 읽어오기   
+        {
+            string line = stringReader.ReadLine(); // 계속 읽기
+
+            if (line == null)
+            {
+                break;
+            }
+
+            //데이터 생성
+            DataSet dataSet = new DataSet();
+            dataSet.time = float.Parse(line.Split(',')[0]);
+            dataSet.enemyHp = int.Parse(line.Split(',')[1]);
+            dataSet.energyProduction = int.Parse(line.Split(',')[2]);
+            dataSet.energyAmount = int.Parse(line.Split(',')[3]);
+            dataSet.shieldOn = int.Parse(line.Split(',')[4]);
+            dataSet.result = int.Parse(line.Split(',')[5]);
+
+            TrainData.Add(dataSet); // TrainData에 나눈 항목들하나씩저장해주기
+
+        }
+
+        stringReader.Close(); //파일 닫기
     }
+
+    public void CalculateDistance()
+    {
+        UserData.time = Random.Range(0, 60);
+        UserData.enemyHp = Random.Range(0, 20);
+        UserData.energyProduction = Random.Range(0, 5);
+        UserData.energyAmount = Random.Range(0, 50);
+        UserData.shieldOn = Random.Range(0, 1);
+
+        Debug.Log("time : " + UserData.time);
+        Debug.Log("eHP : " + UserData.enemyHp);
+        Debug.Log("energyProduction : " + UserData.energyProduction);
+        Debug.Log("energyAmount : " + UserData.energyAmount);
+        Debug.Log("shieldOn : " + UserData.shieldOn);
+
+        distance.Clear();
+
+        for (int i = 0; i < TrainData.Count; i++)
+        {
+            distance.Add(Mathf.Pow(Twice((TrainData[i].time - UserData.time) / 10)
+                + Twice(TrainData[i].enemyHp - UserData.enemyHp)
+                + Twice(TrainData[i].energyProduction - UserData.energyProduction)
+                + Twice((TrainData[i].energyAmount - UserData.energyAmount) / 3)
+                + Twice(TrainData[i].shieldOn - UserData.shieldOn)
+                , 0.5f)); // 거리계산, 재시작횟수 가중치 높히기 위해 *10
+
+        }
+
+        ChooseData();
+    }
+
+    public void ChooseData() // 입력 데이터와 거리가 가까운 K개의 데이터의 레이블을 고르는 함수
+    {
+
+        addResult = 0;
+
+        for (int j = 0; j < kNumber; j++) // k만큼 반복문 실행
+        {
+            for (int i = 0; i < distance.Count - 1; i++) // 주어진 데이터들을 다 비교하기 위한 반복문 
+            {
+                if (distance[i] > distance[i + 1]) // 현재 저장된 가장 가까운 거리가 새로운 거리보다 크다면 갱신
+                {
+                    bestDistance = distance[i + 1];
+                    bestLabel = TrainData[i + 1].result; // 거리가 가장 가까운 놈의 레이블을 저장
+                    countNumber = i + 1; // 몇번쩨 데이터를 저장했는지 알기 위해서 > k만큼 반복을 돌릴때 이전에 결과값은 제외하기 위해                    
+                }
+                else if (distance[i] == distance[i + 1]) // 같아도 갱신
+                {
+                    bestDistance = distance[i + 1];
+                    bestLabel = TrainData[i + 1].result;
+                    countNumber = i + 1;
+                }
+                else
+                {
+                    bestDistance = distance[i];
+                    bestLabel = TrainData[i].result;
+                    countNumber = i;
+                }
+            }
+
+            saveTime[j] = TrainData[countNumber].time; //데이터를 데이터 집합에서 삭제하기전 다시 살리기 위해 백업
+            saveHP[j] = TrainData[countNumber].enemyHp;
+            saveEnergyProduction[j] = TrainData[countNumber].energyProduction;
+            saveEnergy[j] = TrainData[countNumber].energyAmount;
+            saveShield[j] = TrainData[countNumber].shieldOn;
+            saveResult[j] = TrainData[countNumber].result;
+
+            distance.Remove(distance[countNumber]); // 가장 가까운 거리데이터를 제거
+            TrainData.Remove(TrainData[countNumber]); // 가장 가까운 거리데이터의 결과를 가져온 트레인데이터를 제거
+
+            Debug.Log("111"+TrainData.Count);
+
+            labelSet[j] = bestLabel; // 찾아낸 최근접 데이터의 레이블을 수집
+            addResult += labelSet[j]; // 레이블들의 평균을 계산하기 위해 한곳에 계속 더해둠                                
+        }
+
+        bestLabel = Mathf.Round(addResult / kNumber); // 최종적으로 입력 데이터의 레이블을 구함
+        Debug.Log("반올림 직전의 값은? : " + addResult / kNumber);
+        Debug.Log("선택 결과는?? : " + bestLabel);
+
+        for (int i = 0; i < kNumber; i++) // k개의 데이터, 레이블을 고르는 과정에서 삭제한 트레인 데이터를 복구시켜줌
+        {
+            DataSet saveData = new DataSet();
+            saveData.time = saveTime[i];
+            saveData.enemyHp = saveHP[i];
+            saveData.energyProduction = saveEnergyProduction[i];
+            saveData.energyAmount = saveEnergy[i];
+            saveData.shieldOn = saveShield[i];
+            saveData.result = saveResult[i];
+
+            TrainData.Add(saveData);
+        }
+        Debug.Log("222"+TrainData.Count);
+
+    }
+
+    public float Twice(float a)
+    {
+        return a * a;
+    }
+}
+
+[System.Serializable]
+public class DataSet // 데이터를 위한 클래스
+{
+    public float time = 0;
+    public int enemyHp = 0;
+    public int energyProduction = 0;
+    public int energyAmount = 0;
+    public int shieldOn;
+    public float result = 0;
 }
